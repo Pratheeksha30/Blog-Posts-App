a4cdf54088571ed62e4d491304746a1d188a1dfb
Pratheeksha P <84716592+Pratheeksha30@users.noreply.github.com>
2023-05-25 18:53:39 +0530

    Update posts.js

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,11 +1,12 @@
 import connect from '../../lib/mongodb';
 import { Post } from '../../model/schema';
 
-connect();
+connect();      //Connect to the Database
 
 export default async function postHandler(req,res){
 
     if(req.method == 'GET'){
+        // extracting the user email id from query string
         let user = req.query.user;
         user  = new String(user);
         const posts = await Post.find({author : user});
@@ -14,6 +15,7 @@ export default async function postHandler(req,res){
 
     else if(req.method == 'POST'){
         try {
+            // posting the body of request to the 'Post' collection
             const post = await Post.create(req.body);
             console.log(post)
             if(post){
@@ -29,11 +31,13 @@ export default async function postHandler(req,res){
 
     else if(req.method == 'PUT'){
         try {
+            // updating the title , description and Last modified date
             const blog = {
                 title : req.body.title,
                 description : req.body.description,
                 lastUpdatedOn : req.body.lastUpdatedOn
             } 
+            // (author, title) pair is unique key
             const post = await Post.updateOne({author : req.body.author, title : req.body.oldTitle}, {$set : blog});
             if(post){
                 return res.send({message: "Successful"});
@@ -48,6 +52,8 @@ export default async function postHandler(req,res){
 
     else if(req.method == 'DELETE'){
         try {
+            
+            // Delete a blog post using (author,title) as key
             const post = await Post.deleteOne({author : req.body.author, title : req.body.oldTitle});
             console.log(post);
             if(post){
